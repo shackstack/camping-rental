5b01b93b5a387b0a31742a3a70c87289898177b2
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-04-13 18:51:31 +0900

    chore(#3): 타입 동기회 스크립트 실행시 환경변수 참조가능하도록 설정

--- openapi.ts
+++ openapi.ts
@@ -1,5 +1,7 @@
 import fs from "node:fs";
 import openapiTS, { astToString } from "openapi-typescript";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
 
 const SCHEMA_URL = `${process.env.VITE_API_SERVER_BASE_URL}/v3/api-docs`;
 
