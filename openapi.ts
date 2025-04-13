3e0cdb99ae3431c07367fa593b3ec7783b0da3a4
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-04-13 18:49:39 +0900

    chore(#3): openapi typescript 타입 동기화 시스템 구축

---
+++ openapi.ts
@@ -0,0 +1,8 @@
+import fs from "node:fs";
+import openapiTS, { astToString } from "openapi-typescript";
+
+const SCHEMA_URL = `${process.env.VITE_API_SERVER_BASE_URL}/v3/api-docs`;
+
+const ast = await openapiTS(SCHEMA_URL, {});
+
+fs.writeFileSync("./src/types/server/index.d.ts", astToString(ast));
