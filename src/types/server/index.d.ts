e33f32fbb3dbbcfa9f0a5af496ec143c75d2c6f2
김민석/E <jeremy@heyratel.com>
2025-04-25 14:30:30 +0900

    feat(#3): 소셜 로그인 및 헬스체크 관련 타입 정의 수정

--- src/types/server/index.d.ts
+++ src/types/server/index.d.ts
@@ -1,247 +1,247 @@
 export interface paths {
-  "/v1/api/refresh-token": {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
+    "/v1/api/refresh-token": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        /** 토큰 재발행 */
+        post: operations["refresh"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-    get?: never;
-    put?: never;
-    /** 토큰 재발행 */
-    post: operations["refresh"];
-    delete?: never;
-    options?: never;
-    head?: never;
-    patch?: never;
-    trace?: never;
-  };
-  "/v1/api/members/social": {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
+    "/v1/api/members/social": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        /** SNS 회원 가입 */
+        post: operations["join"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-    get?: never;
-    put?: never;
-    /** SNS 회원 가입 */
-    post: operations["join"];
-    delete?: never;
-    options?: never;
-    head?: never;
-    patch?: never;
-    trace?: never;
-  };
-  "/v1/api/login/social": {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
+    "/v1/api/login/social": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        /** SNS 로그인 */
+        post: operations["join_1"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-    get?: never;
-    put?: never;
-    /** SNS 로그인 */
-    post: operations["join_1"];
-    delete?: never;
-    options?: never;
-    head?: never;
-    patch?: never;
-    trace?: never;
-  };
-  "/v1/api/members/social/url": {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
+    "/v1/api/members/social/url": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** SNS 회원 가입 로그인 URL */
+        get: operations["create"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-    /** SNS 회원 가입 로그인 URL */
-    get: operations["create"];
-    put?: never;
-    post?: never;
-    delete?: never;
-    options?: never;
-    head?: never;
-    patch?: never;
-    trace?: never;
-  };
-  "/": {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
+    "/": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** 헬스체크 */
+        get: operations["healthCheck"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-    /** 헬스체크 */
-    get: operations["healthCheck"];
-    put?: never;
-    post?: never;
-    delete?: never;
-    options?: never;
-    head?: never;
-    patch?: never;
-    trace?: never;
-  };
 }
 export type webhooks = Record<string, never>;
 export interface components {
-  schemas: {
-    RefreshTokenRequest: {
-      accessToken: string;
-      refreshToken: string;
+    schemas: {
+        RefreshTokenRequest: {
+            accessToken: string;
+            refreshToken: string;
+        };
+        RefreshTokenResponse: {
+            accessToken: string;
+            refreshToken: string;
+        };
+        JoinSocialRequest: {
+            /** @enum {string} */
+            joinType: "KAKAO" | "NAVER";
+            socialAccessToken: string;
+        };
+        JoinSocialResponse: {
+            accessToken: string;
+            refreshToken: string;
+        };
+        LoginSocialRequest: {
+            socialAccessToken: string;
+            /** @enum {string} */
+            loginType: "KAKAO" | "NAVER";
+            remoteAddress?: string;
+            deviceInformation?: string;
+            appVersion?: string;
+        };
+        LoginSocialResponse: {
+            accessToken: string;
+            refreshToken: string;
+        };
+        CreateOauthUrlResponse: {
+            url: string;
+        };
+        Unit: Record<string, never>;
     };
-    RefreshTokenResponse: {
-      accessToken: string;
-      refreshToken: string;
-    };
-    JoinSocialRequest: {
-      /** @enum {string} */
-      joinType: "KAKAO" | "NAVER";
-      socialId: string;
-    };
-    JoinSocialResponse: {
-      accessToken: string;
-      refreshToken: string;
-    };
-    LoginSocialRequest: {
-      socialId: string;
-      /** @enum {string} */
-      loginTypeDto: "KAKAO" | "NAVER";
-      remoteAddress?: string;
-      deviceInformation?: string;
-      appVersion?: string;
-    };
-    LoginSocialResponse: {
-      accessToken: string;
-      refreshToken: string;
-    };
-    CreateOauthUrlResponse: {
-      url: string;
-    };
-    Unit: Record<string, never>;
-  };
-  responses: never;
-  parameters: never;
-  requestBodies: never;
-  headers: never;
-  pathItems: never;
+    responses: never;
+    parameters: never;
+    requestBodies: never;
+    headers: never;
+    pathItems: never;
 }
 export type $defs = Record<string, never>;
 export interface operations {
-  refresh: {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
-    };
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["RefreshTokenRequest"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        headers: {
-          [name: string]: unknown;
+    refresh: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
         };
-        content: {
-          "*/*": components["schemas"]["RefreshTokenResponse"];
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["RefreshTokenRequest"];
+            };
         };
-      };
-    };
-  };
-  join: {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
-    };
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["JoinSocialRequest"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        headers: {
-          [name: string]: unknown;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["RefreshTokenResponse"];
+                };
+            };
         };
-        content: {
-          "*/*": components["schemas"]["JoinSocialResponse"];
+    };
+    join: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
         };
-      };
-    };
-  };
-  join_1: {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
-    };
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["LoginSocialRequest"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        headers: {
-          [name: string]: unknown;
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["JoinSocialRequest"];
+            };
         };
-        content: {
-          "*/*": components["schemas"]["LoginSocialResponse"];
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["JoinSocialResponse"];
+                };
+            };
         };
-      };
     };
-  };
-  create: {
-    parameters: {
-      query: {
-        type: "KAKAO" | "NAVER";
-      };
-      header?: never;
-      path?: never;
-      cookie?: never;
-    };
-    requestBody?: never;
-    responses: {
-      /** @description OK */
-      200: {
-        headers: {
-          [name: string]: unknown;
+    join_1: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
         };
-        content: {
-          "*/*": components["schemas"]["CreateOauthUrlResponse"];
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["LoginSocialRequest"];
+            };
         };
-      };
-    };
-  };
-  healthCheck: {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
-    };
-    requestBody?: never;
-    responses: {
-      /** @description OK */
-      200: {
-        headers: {
-          [name: string]: unknown;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["LoginSocialResponse"];
+                };
+            };
         };
-        content: {
-          "*/*": components["schemas"]["Unit"];
-        };
-      };
     };
-  };
+    create: {
+        parameters: {
+            query: {
+                type: "KAKAO" | "NAVER";
+            };
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["CreateOauthUrlResponse"];
+                };
+            };
+        };
+    };
+    healthCheck: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["Unit"];
+                };
+            };
+        };
+    };
 }
