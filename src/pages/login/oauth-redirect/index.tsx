f4b0eff41dfbdb09826255a5fe7396cdc191500b
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-04-27 12:33:04 +0900

    feat(#3): 네이버 로그인 구현

--- src/pages/login/oauth-redirect/index.tsx
+++ src/pages/login/oauth-redirect/index.tsx
@@ -4,7 +4,7 @@ import { https } from "../../../apis/instance";
 import axios from "axios";
 import { components } from "../../../types/server";
 
-const login = async ({ code }: { code: string }) => {
+const loginKakao = async ({ code }: { code: string }) => {
   const { data } = await axios.post(
     "https://kauth.kakao.com/oauth/token",
     {
@@ -39,14 +39,55 @@ const login = async ({ code }: { code: string }) => {
   window.location.href = "/";
 };
 
+const loginNaver = async ({ code }: { code: string }) => {
+  const { data } = await axios.post(
+    "https://nid.naver.com/oauth2.0/token",
+    {
+      grant_type: "authorization_code",
+      client_id: import.meta.env.VITE_NAVER_CLIENT_ID,
+      client_secret: import.meta.env.VITE_NAVER_CLIENT_SECRET,
+      code,
+    },
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
+      },
+    },
+  );
+
+  const { accessToken, refreshToken }: components["schemas"]["JoinSocialResponse"] = await https.post(
+    "/members/social",
+    {
+      socialAccessToken: data.access_token,
+      joinType: "NAVER",
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  localStorage.setItem("accessToken", accessToken);
+  localStorage.setItem("refreshToken", refreshToken);
+
+  window.location.href = "/";
+};
+
 const OauthRedirectPage = () => {
   const [searchParams] = useSearchParams();
 
   useEffect(() => {
-    if (searchParams.has("code")) {
-      login({ code: searchParams.get("code")! });
+    if (!searchParams.has("code")) return;
+
+    if (searchParams.get("join-type") === "KAKAO") {
+      loginKakao({ code: searchParams.get("code")! });
     }
-  }, []);
+
+    if (searchParams.get("join-type") === "NAVER") {
+      loginNaver({ code: searchParams.get("code")! });
+    }
+  }, [searchParams.get("code")]);
 
   return <div>로그인 처리중</div>;
 };
