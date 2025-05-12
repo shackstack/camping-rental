bcf5fca20517946359bc2b803acbd955f958d978
Jeremy <102432453+shackstack@users.noreply.github.com>
2025-05-12 23:36:00 +0900

    feat(#4): SNS 회원 가입 여부 확인 API 추가 및 타입 정의 업데이트

--- src/types/server/index.d.ts
+++ src/types/server/index.d.ts
@@ -1,378 +1,427 @@
 export interface paths {
-  "/v1/api/refresh-token": {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
+    "/v1/api/refresh-token": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        /** 토큰 재발행 */
+        post: operations["refresh"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-    get?: never;
-    put?: never;
-    /** 토큰 재발행 */
-    post: operations["refresh"];
-    delete?: never;
-    options?: never;
-    head?: never;
-    patch?: never;
-    trace?: never;
-  };
-  "/v1/api/members/social": {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
+    "/v1/api/members/social": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        /** SNS 회원 가입 */
+        post: operations["join"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-    get?: never;
-    put?: never;
-    /** SNS 회원 가입 */
-    post: operations["join"];
-    delete?: never;
-    options?: never;
-    head?: never;
-    patch?: never;
-    trace?: never;
-  };
-  "/v1/api/login/social": {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
+    "/v1/api/members/social/status": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        /** SNS 회원 가입 여부 확인 */
+        post: operations["check"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-    get?: never;
-    put?: never;
-    /** SNS 로그인 */
-    post: operations["join_1"];
-    delete?: never;
-    options?: never;
-    head?: never;
-    patch?: never;
-    trace?: never;
-  };
-  "/v1/api/products": {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
+    "/v1/api/login/social": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        /** SNS 로그인 */
+        post: operations["join_1"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-    /** 상품 리스트 조회 */
-    get: operations["query"];
-    put?: never;
-    post?: never;
-    delete?: never;
-    options?: never;
-    head?: never;
-    patch?: never;
-    trace?: never;
-  };
-  "/v1/api/products/{id}": {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
+    "/v1/api/products": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** 상품 리스트 조회 */
+        get: operations["query"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-    /** 상품 상세 조회 */
-    get: operations["query_1"];
-    put?: never;
-    post?: never;
-    delete?: never;
-    options?: never;
-    head?: never;
-    patch?: never;
-    trace?: never;
-  };
-  "/v1/api/members/social/url": {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
+    "/v1/api/products/{id}": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** 상품 상세 조회 */
+        get: operations["query_1"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-    /** SNS 회원 가입 로그인 URL */
-    get: operations["create"];
-    put?: never;
-    post?: never;
-    delete?: never;
-    options?: never;
-    head?: never;
-    patch?: never;
-    trace?: never;
-  };
-  "/": {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
+    "/v1/api/members/social/url": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** SNS 회원 가입 로그인 URL */
+        get: operations["create"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
     };
-    /** 헬스체크 */
-    get: operations["healthCheck"];
-    put?: never;
-    post?: never;
-    delete?: never;
-    options?: never;
-    head?: never;
-    patch?: never;
-    trace?: never;
-  };
+    "/": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** 헬스체크 */
+        get: operations["healthCheck"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
-  schemas: {
-    RefreshTokenRequest: {
-      accessToken: string;
-      refreshToken: string;
+    schemas: {
+        RefreshTokenRequest: {
+            accessToken: string;
+            refreshToken: string;
+        };
+        RefreshTokenResponse: {
+            accessToken: string;
+            refreshToken: string;
+        };
+        JoinSocialRequest: {
+            /** @enum {string} */
+            joinType: "KAKAO" | "NAVER";
+            socialAccessToken: string;
+        };
+        JoinSocialResponse: {
+            accessToken: string;
+            refreshToken: string;
+        };
+        CheckSocialMemberExistRequest: {
+            /** @enum {string} */
+            joinType: "KAKAO" | "NAVER";
+            accessToken: string;
+        };
+        CheckSocialMemberExistResponse: {
+            exists: boolean;
+        };
+        LoginSocialRequest: {
+            socialAccessToken: string;
+            /** @enum {string} */
+            loginType: "KAKAO" | "NAVER";
+            remoteAddress?: string;
+            deviceInformation?: string;
+            appVersion?: string;
+        };
+        LoginSocialResponse: {
+            accessToken: string;
+            refreshToken: string;
+        };
+        PriceResponse: {
+            /** Format: int64 */
+            originalPrice: number;
+            /** Format: int64 */
+            disCountedPrice: number;
+            /** Format: double */
+            discountRate: number;
+        };
+        QueryProductsResponse: {
+            /** Format: int64 */
+            id: number;
+            /** Format: int64 */
+            cursorIndex: number;
+            code: string;
+            representativeImageUploadFileName: string;
+            title: string;
+            member: components["schemas"]["SimpleMemberResponse"];
+            price: components["schemas"]["PriceResponse"];
+        };
+        SimpleMemberResponse: {
+            nickname: string;
+            uid: string;
+        };
+        OptionChoiceResponse: {
+            /** Format: int64 */
+            id: number;
+            name: string;
+            /** Format: int64 */
+            additionalPrice: number;
+        };
+        OptionResponse: {
+            /** Format: int64 */
+            id: number;
+            title: string;
+            choices: components["schemas"]["OptionChoiceResponse"][];
+        };
+        QueryProductDetailResponse: {
+            /** Format: int64 */
+            id: number;
+            code: string;
+            imageUrls: string[];
+            detailImageUrls: string[];
+            title: string;
+            member: components["schemas"]["SimpleMemberResponse"];
+            price: components["schemas"]["PriceResponse"];
+            description: string;
+            possibleDeliveryTypes: ("COURIER" | "SELF_PICKUP")[];
+            /** Format: int64 */
+            stockCount: number;
+            bundles: string[];
+            options: components["schemas"]["OptionResponse"][];
+        };
+        CreateOauthUrlResponse: {
+            url: string;
+        };
+        Unit: Record<string, never>;
     };
-    RefreshTokenResponse: {
-      accessToken: string;
-      refreshToken: string;
-    };
-    JoinSocialRequest: {
-      /** @enum {string} */
-      joinType: "KAKAO" | "NAVER";
-      socialAccessToken: string;
-    };
-    JoinSocialResponse: {
-      accessToken: string;
-      refreshToken: string;
-    };
-    LoginSocialRequest: {
-      socialAccessToken: string;
-      /** @enum {string} */
-      loginType: "KAKAO" | "NAVER";
-      remoteAddress?: string;
-      deviceInformation?: string;
-      appVersion?: string;
-    };
-    LoginSocialResponse: {
-      accessToken: string;
-      refreshToken: string;
-    };
-    PriceResponse: {
-      /** Format: int64 */
-      originalPrice: number;
-      /** Format: int64 */
-      disCountedPrice: number;
-      /** Format: double */
-      discountRate: number;
-    };
-    QueryProductsResponse: {
-      /** Format: int64 */
-      id: number;
-      /** Format: int64 */
-      cursorIndex: number;
-      code: string;
-      representativeImageUploadFileName: string;
-      title: string;
-      member: components["schemas"]["SimpleMemberResponse"];
-      price: components["schemas"]["PriceResponse"];
-    };
-    SimpleMemberResponse: {
-      nickname: string;
-      uid: string;
-    };
-    OptionChoiceResponse: {
-      /** Format: int64 */
-      id: number;
-      name: string;
-      /** Format: int64 */
-      additionalPrice: number;
-    };
-    OptionResponse: {
-      /** Format: int64 */
-      id: number;
-      title: string;
-      choices: components["schemas"]["OptionChoiceResponse"][];
-    };
-    QueryProductDetailResponse: {
-      /** Format: int64 */
-      id: number;
-      code: string;
-      imageUrls: string[];
-      detailImageUrls: string[];
-      title: string;
-      member: components["schemas"]["SimpleMemberResponse"];
-      price: components["schemas"]["PriceResponse"];
-      description: string;
-      possibleDeliveryTypes: ("COURIER" | "SELF_PICKUP")[];
-      /** Format: int64 */
-      stockCount: number;
-      bundles: string[];
-      options: components["schemas"]["OptionResponse"][];
-    };
-    CreateOauthUrlResponse: {
-      url: string;
-    };
-    Unit: Record<string, never>;
-  };
-  responses: never;
-  parameters: never;
-  requestBodies: never;
-  headers: never;
-  pathItems: never;
+    responses: never;
+    parameters: never;
+    requestBodies: never;
+    headers: never;
+    pathItems: never;
 }
 export type $defs = Record<string, never>;
 export interface operations {
-  refresh: {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
-    };
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["RefreshTokenRequest"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        headers: {
-          [name: string]: unknown;
+    refresh: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
         };
-        content: {
-          "*/*": components["schemas"]["RefreshTokenResponse"];
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["RefreshTokenRequest"];
+            };
         };
-      };
-    };
-  };
-  join: {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
-    };
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["JoinSocialRequest"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        headers: {
-          [name: string]: unknown;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["RefreshTokenResponse"];
+                };
+            };
         };
-        content: {
-          "*/*": components["schemas"]["JoinSocialResponse"];
+    };
+    join: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
         };
-      };
-    };
-  };
-  join_1: {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
-    };
-    requestBody: {
-      content: {
-        "application/json": components["schemas"]["LoginSocialRequest"];
-      };
-    };
-    responses: {
-      /** @description OK */
-      200: {
-        headers: {
-          [name: string]: unknown;
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["JoinSocialRequest"];
+            };
         };
-        content: {
-          "*/*": components["schemas"]["LoginSocialResponse"];
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["JoinSocialResponse"];
+                };
+            };
         };
-      };
     };
-  };
-  query: {
-    parameters: {
-      query?: {
-        cursorIndex?: number;
-        size?: number;
-      };
-      header?: never;
-      path?: never;
-      cookie?: never;
-    };
-    requestBody?: never;
-    responses: {
-      /** @description OK */
-      200: {
-        headers: {
-          [name: string]: unknown;
+    check: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
         };
-        content: {
-          "*/*": components["schemas"]["QueryProductsResponse"][];
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["CheckSocialMemberExistRequest"];
+            };
         };
-      };
-    };
-  };
-  query_1: {
-    parameters: {
-      query?: never;
-      header?: never;
-      path: {
-        id: number;
-      };
-      cookie?: never;
-    };
-    requestBody?: never;
-    responses: {
-      /** @description OK */
-      200: {
-        headers: {
-          [name: string]: unknown;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["CheckSocialMemberExistResponse"];
+                };
+            };
         };
-        content: {
-          "*/*": components["schemas"]["QueryProductDetailResponse"];
-        };
-      };
     };
-  };
-  create: {
-    parameters: {
-      query: {
-        type: "KAKAO" | "NAVER";
-      };
-      header?: never;
-      path?: never;
-      cookie?: never;
-    };
-    requestBody?: never;
-    responses: {
-      /** @description OK */
-      200: {
-        headers: {
-          [name: string]: unknown;
+    join_1: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
         };
-        content: {
-          "*/*": components["schemas"]["CreateOauthUrlResponse"];
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["LoginSocialRequest"];
+            };
         };
-      };
-    };
-  };
-  healthCheck: {
-    parameters: {
-      query?: never;
-      header?: never;
-      path?: never;
-      cookie?: never;
-    };
-    requestBody?: never;
-    responses: {
-      /** @description OK */
-      200: {
-        headers: {
-          [name: string]: unknown;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["LoginSocialResponse"];
+                };
+            };
         };
-        content: {
-          "*/*": components["schemas"]["Unit"];
-        };
-      };
     };
-  };
+    query: {
+        parameters: {
+            query?: {
+                cursorIndex?: number;
+                size?: number;
+            };
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["QueryProductsResponse"][];
+                };
+            };
+        };
+    };
+    query_1: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path: {
+                id: number;
+            };
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["QueryProductDetailResponse"];
+                };
+            };
+        };
+    };
+    create: {
+        parameters: {
+            query: {
+                type: "KAKAO" | "NAVER";
+            };
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["CreateOauthUrlResponse"];
+                };
+            };
+        };
+    };
+    healthCheck: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description OK */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "*/*": components["schemas"]["Unit"];
+                };
+            };
+        };
+    };
 }
